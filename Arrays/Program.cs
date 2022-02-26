f28ddc1ef68fb5e9750c0b97f238d38744e4a893
osmankaraoglu <osmnkaraoglu@gmail.com>
2022-02-26 19:05:30 +0300

    Added Arrays Sample

---
+++ Arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //dizi tanımlama
+            string[] renkler = new string[5];
+            string[] hayvanlar = { "kedi", "köpek", "kuş", "maymun" };
+
+            int[] dizi;
+            dizi = new int[5];
+
+            //değer atama
+            renkler[0] = "Mavi";
+            dizi[3] = 10;
+
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            //döngülerde dizi kullanımı
+            //klavyeden girilen n tane sayının ortalamasını hesaplayan program
+            Console.WriteLine("Lütfen dizinin eleman sayısını giriniz:");
+            int diziBoyut = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziBoyut];
+
+            for (int i = 0; i < diziBoyut; i++)
+            {
+                Console.Write("{0}. sayıyı giriniz:", i + 1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+            int toplam = 0;
+            foreach (var sayi in sayiDizisi)
+            {
+                toplam += sayi;
+            }
+            Console.WriteLine("Ortalama:" + toplam / diziBoyut);    
+        }
+    }
+}
