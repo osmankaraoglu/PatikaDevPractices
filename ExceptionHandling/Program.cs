b17d42b5ffbafc974fbf45d92cb501a15f2a7f76
osmankaraoglu <osmnkaraoglu@gmail.com>
2022-02-26 11:32:51 +0300

    Added ExceptionHandling Sample

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir Sayı Giriniz:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girilen Sayı:" + sayi);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hata:" + ex.Message.ToString());
+            }
+            //finally
+            //{
+            //    Console.WriteLine("İşlem Tamamlandı.");
+            //}
+
+            try
+            {
+                int a = int.Parse(null);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Boş Değer girdiniz.");
+                Console.WriteLine(ex);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Veri Tipi Uygun Değil.");
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük yada çok büyük bir değer girdiniz.");
+            }
+
+        }
+    }
+}
