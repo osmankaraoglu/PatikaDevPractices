8f0dee9ce0d9b0024121952c6c27ded6c4f2b59e
osmankaraoglu <osmnkaraoglu@gmail.com>
2022-02-27 12:32:51 +0300

    Added Methods Sample

---
+++ Methods/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //erişim_belirteci geri_dönüş_tipi methodAdi parametre
+            //{
+            // komutlar
+            // return
+            //}
+
+            int a = 2;
+            int b = 3;
+
+            int sonuc = Topla(a, b);
+
+            Console.WriteLine(sonuc);
+
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+            int sonuc2 = ornek.ArttirVeTopla(a, b);
+            //ref a, ref b
+            ornek.EkranaYazdir(Convert.ToString(sonuc2));
+        }
+
+        static int Topla(int s1,int s2)
+        {
+            return s1 + s2;
+        }
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttirVeTopla(int s1,int s2)
+            //(ref int s1, ref int s2)
+        {
+            s1++;
+            s2++;
+            return s1+s2;
+        }
+    }
+}
