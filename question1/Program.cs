8749cc8d0525502e107d06b2eb7eecfe6c133421
osmankaraoglu <osmnkaraoglu@gmail.com>
2022-02-27 15:50:41 +0300

    question 1

---
+++ question1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace question1
+{
+    class Program
+    {
+        //Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n).
+        //Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
+        static void Main(string[] args)
+        {
+            int n;
+            while (true)
+            {
+                Console.WriteLine("Lütfen Pozitif Bir Sayı Giriniz:");
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n > 0)
+                    break;
+            }
+
+            int num, i=1;
+            int[] nums = new int[n];
+            Console.WriteLine(n + " Adet Pozitif Sayı Giriniz:");
+            while(i <= n)
+            {
+                Console.WriteLine(i + ". Sayıyı Giriniz:");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num > 0)
+                    nums[i-1] = num;
+                else
+                    continue;
+                i++;
+            }
+
+            foreach(var item in nums){
+                if (item % 2 == 0)
+                    Console.Write(item + " ");
+            }
+        }
+    }
+}
